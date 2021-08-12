cdef3c310146171d197ee1426f9dd304301113e2
Liza <example@gmail.com>
2021-08-12 18:06:31 +0300

    add files

---
+++ mod3_lesson2_step7.py
@@ -0,0 +1,4 @@
+def test_input_text(expected_result, actual_result):
+    # ваша реализация, напишите assert и сообщение об ошибке
+assert expected_result == actual_result, 
+    f"Wrong result, got {actual_result} instead of {expected_result}"  
